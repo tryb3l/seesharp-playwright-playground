dc77665f3d0e7338c53e1e7b444030ce8a078478
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-22 22:33:57 +0200

    Add ConfigSettings class to load configuration from appsettings.json

---
+++ Playground/Config/ConfigSettings.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Playground.Config;
+public class ConfigSettings
+{
+    public required string Browser { get; set; }
+    public required string BaseUrl { get; set; }
+    public bool Headless { get; set; }
+
+    private static ConfigSettings? _config;
+
+    public static ConfigSettings LoadConfig()
+    {
+        if (_config == null)
+        {
+            var baseDirectory = AppContext.BaseDirectory;
+            var projectRoot = (Directory.GetParent(baseDirectory)?.Parent?.Parent?.Parent?.FullName) ?? throw new InvalidOperationException("Project root directory could not be determined.");
+            var configPath = Path.Combine(projectRoot, "Config", "appsettings.json");
+            var configText = File.ReadAllText(configPath);
+            _config = JsonConvert.DeserializeObject<ConfigSettings>(configText);
+        }
+        return _config!;
+    }
+}
