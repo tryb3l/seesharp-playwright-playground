9f3b0fbae4b63e2a54166be8dd0e4cd6a4905ebd
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-22 22:35:24 +0200

    Add BasePage class as a base for all page classes

---
+++ Playground/Pages/BasePage.cs
@@ -0,0 +1,14 @@
+using Microsoft.Playwright;
+using Playground.Config;
+using Serilog;
+
+namespace Playground.Pages;
+
+public abstract class BasePage(IPage page)
+{
+    protected readonly IPage? Page = page;
+    protected readonly ILogger Logger = Log.Logger;
+    protected readonly string BaseUrl = ConfigSettings.LoadConfig().BaseUrl;
+
+    public abstract Task NavigateAsync();
+}
