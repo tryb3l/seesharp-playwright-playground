c6348cd324c6c395f62c48e04edefbc6115999aa
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-22 22:34:30 +0200

    Add PageFactory class to create page instances

---
+++ Playground/Factories/PageFactory.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Playwright;
+using Playground.Pages;
+
+namespace Playground.Factories;
+
+public class PageFactory(IPage page)
+{
+    private readonly IPage _page = page;
+
+    public T GetPage<T>() where T : BasePage
+    {
+        return (T)Activator.CreateInstance(typeof(T), _page)! ?? throw new InvalidOperationException("Failed to create page instance.");
+    }
+}
