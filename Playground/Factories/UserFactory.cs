a4332d198897db795ecc012ff22421c5f32c4843
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-22 22:34:59 +0200

    Add UserFactory class to create and manage user instances via API

---
+++ Playground/Factories/UserFactory.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Playground.Config;
+using Serilog;
+
+namespace Playground.Factories;
+
+public class UserFactory
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILogger _logger;
+
+    public UserFactory()
+    {
+        _httpClient = new HttpClient { BaseAddress = new Uri(ConfigSettings.LoadConfig().BaseUrl) };
+        _logger = Log.Logger;
+    }
+
+    public async Task<string> CreateUserAsync(string email, string password)
+    {
+        var payload = new
+        {
+            user = new
+            {
+                username = email.Split('@')[0],
+                email = email,
+                password = password
+            }
+        };
+
+        try
+        {
+            _logger.Information("Creating user via API with email '{Email}'", email);
+            var response = await _httpClient.PostAsJsonAsync("/api/users", payload);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadFromJsonAsync<dynamic>();
+            string token = content!.user.token;
+
+            _logger.Information("User created and token received");
+            return token;
+        }
+        catch (Exception e)
+        {
+            _logger.Error(e, "Failed to create user via API.");
+            throw;
+        }
+    }
+
+    public async Task<string> LoginUserAsync(string email, string password)
+    {
+        var payload = new
+        {
+            user = new
+            {
+                email = email,
+                password = password
+            }
+        };
+        try
+        {
+            _logger.Information("Logging in user via API with email '{Email}'", email);
+            var response = await _httpClient.PostAsJsonAsync("/api/users/login", payload);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadFromJsonAsync<dynamic>();
+            string token = content!.user.token;
+
+            _logger.Information("User logged in and token received");
+            _logger.Information("User logged in and token received");
+            return token;
+        }
+        catch (Exception e)
+        {
+            _logger.Error(e, "Failed to log in user via API");
+            throw;
+        }
+    }
+}
