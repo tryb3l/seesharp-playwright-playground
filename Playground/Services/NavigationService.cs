5859ae9195ea555f56c625eb6785c8670e02d78b
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-22 22:58:21 +0200

    code clean up

--- Playground/Services/NavigationService.cs
+++ Playground/Services/NavigationService.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.Playwright;
 using Playground.Pages;
 
@@ -8,7 +7,7 @@ public class NavigationService(IPage page)
 {
     private readonly IPage _page = page;
 
-    public async Task<T>NavigateToAsync<T>() where T: BasePage
+    public async Task<T> NavigateToAsync<T>() where T : BasePage
     {
         var pageInstance = (T)Activator.CreateInstance(typeof(T), _page)! ?? throw new InvalidOperationException("Failed to create page instance.");
         await pageInstance.NavigateAsync();
