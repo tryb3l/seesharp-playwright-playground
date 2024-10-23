564a70e7008bfe5b5903545b2195a92bc5579cc7
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-23 13:51:37 +0200

    provided changes to page instance creation, added specific page naviation methods

--- Playground/Services/NavigationService.cs
+++ Playground/Services/NavigationService.cs
@@ -1,16 +1,26 @@
 using Microsoft.Playwright;
-using Playground.Pages;
+using Playground.Components;
+using Playground.Config;
 
 namespace Playground.Services;
 
 public class NavigationService(IPage page)
 {
     private readonly IPage _page = page;
+    private readonly HeaderComponent _headerComponent = new(page);
 
-    public async Task<T> NavigateToAsync<T>() where T : BasePage
+    public async Task GoToLoginPageAsync()
     {
-        var pageInstance = (T)Activator.CreateInstance(typeof(T), _page)! ?? throw new InvalidOperationException("Failed to create page instance.");
-        await pageInstance.NavigateAsync();
-        return pageInstance;
+        await _headerComponent.ClickSignInAsync();
+    }
+
+    public async Task GoToHomePageAsync()
+    {
+        await _page.GotoAsync($"{ConfigSettings.LoadConfig().BaseUrl}/");
+    }
+
+    public async Task GoToSignUpPageAsync()
+    {
+        await _headerComponent.ClickSignUpAsync();
     }
 }
