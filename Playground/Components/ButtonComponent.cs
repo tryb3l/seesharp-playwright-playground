d573e0f3316f4d841645af1f0453fcd8ed135ceb
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-23 13:47:48 +0200

    imports optimization, extended logging in tests

--- Playground/Components/ButtonComponent.cs
+++ Playground/Components/ButtonComponent.cs
@@ -1,4 +1,5 @@
 using Microsoft.Playwright;
+using Playground.Base;
 
 namespace Playground.Components;
 
@@ -10,34 +11,50 @@ public class ButtonComponent(IPage page) : BaseComponent
     {
         try
         {
-            Logger.Information("Clicking button with selector '{Selector}'", selector);
+            Logger.Information("Clicking element with selector '{Selector}'", selector);
+
             await Page.ClickAsync(selector);
-            Logger.Information("Successfully clicked button with selector '{Selector}'", selector);
+
+            Logger.Information("Successfully clicked element with selector '{Selector}'", selector);
         }
         catch (Exception e)
         {
-            Logger.Error(e, "Failed to click button with selector '{Selector}'", selector);
+            Logger.Error(e, "Failed to click element with selector '{Selector}'", selector);
             throw;
         }
     }
 
-    public async Task ClickByTextAsync(string buttonText)
+    public async Task ClickByTextAsync(string elementText)
     {
         try
         {
-            Logger.Information("Clicking button with text '{ButtonText}'", buttonText);
-            await Page.ClickAsync($"text='{buttonText}'");
-            Logger.Information("Successfully clicked button with text '{ButtonText}'", buttonText);
+            Logger.Information("Clicking element with text '{ButtonText}'", elementText);
+
+            await Page.ClickAsync($"text='{elementText}'");
+
+            Logger.Information("Successfully clicked element with text '{ButtonText}'", elementText);
         }
         catch (Exception e)
         {
-            Logger.Error(e, "Failed to click button with text '{ButtonText}'", buttonText);
+            Logger.Error(e, "Failed to click element with text '{ElementText}'", elementText);
             throw;
         }
     }
 
     public async Task ClickByRoleAsync(string role, string name)
     {
-        await Page.GetByRole(Enum.Parse<AriaRole>(role), new() { Name = name }).ClickAsync();
+        try
+        {
+            Logger.Information("Clicking element with role '{Role}' and name {Name}", role, name);
+
+            await Page.GetByRole(Enum.Parse<AriaRole>(role), new() { Name = name }).ClickAsync();
+
+            Logger.Information("Successfully clicked an element with role '{Role}' and name {Name}", role, name);
+        }
+        catch (Exception e)
+        {
+            Logger.Error(e, "Failed to click element with name '{Name}' and role '{Role}'", name);
+        }
+
     }
 }
